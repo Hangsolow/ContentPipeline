67ad17b6d7b6abe12acbb14bdc78bdce58ea163e
Alex Boesen <alex@await.io>
2023-07-20 17:21:59 +0200

    fixes errors found by EnforceExtendedAnalyzerRules

---
+++ src/ContentPipeline/CodeBuilders/CodeConsts.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContentPipeline.CodeBuilders;
+internal static class CodeConsts
+{
+    internal const string NewLine = "\n";
+}
