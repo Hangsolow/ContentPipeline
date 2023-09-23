2eeeb5d14d37a2d6648f0a5b43b33d63c91c9e27
Alex Boesen <alex@await.io>
2023-09-23 13:04:59 +0200

    removes old unused code

--- src/ContentPipeline/SourceGenerator/Parser.cs
+++ src/ContentPipeline/SourceGenerator/Parser.cs
@@ -109,28 +109,3 @@ internal sealed partial class Parser
         }
     }
 }
-
-internal class ContentClassComparer : IComparer<ContentClass>
-{
-    public int Compare(ContentClass x, ContentClass y)
-    {
-        if (ReferenceEquals(x, y))
-        {
-            return 0;
-        }
-
-        if (ReferenceEquals(null, y))
-        {
-            return 1;
-        }
-
-        if (ReferenceEquals(null, x))
-        {
-            return -1;
-        }
-
-        return x.Order.CompareTo(y.Order);
-    }
-
-    public static ContentClassComparer Default { get; } = new ContentClassComparer();
-}
